0e40e8a9fcd180af36cc9617bfce6e266a73bc75
YashS0ni <soniyash6606@gmail.com>
2024-07-29 16:04:25 +0530

    changes made in routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,54 +1,47 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Signin from "../views/Signin.vue";
-import Signup from "../views/Signup.vue";
-import baseRoutes from "./baseRoutes.js";
-import { useStore } from "vuex";
+import { createRouter, createWebHistory } from 'vue-router'
+import Signin from '../views/Signin.vue'
+import Signup from '../views/Signup.vue'
+import baseRoutes from './baseRoutes.js'
+import { useStore } from 'vuex'
 
-const store = useStore();
+const store = useStore()
 
 const routes = [
-
-
   {
-    path: "/",
-    name: "/",
-    redirect: "/Signin",
+    path: '/',
+    name: '/',
+    redirect: '/Signin',
     meta: { requiresAuth: true },
-    children:baseRoutes,
-    
+    children: baseRoutes,
   },
   {
-    path: "/Signin",
-    name: "Signin",
+    path: '/Signin',
+    name: 'Signin',
     component: Signin,
-  
   },
   {
-    path: "/Signup",
-    name: "Signup",
+    path: '/Signup',
+    name: 'Signup',
     component: Signup,
-    
   },
-
-  
-  
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-  linkActiveClass: "active",
-});
+  linkActiveClass: 'active',
+})
 
 router.beforeEach((to, from, next) => {
- 
-  if (to.meta.requiresAuth && store.state.authenticated==false) {
-    next('/Signin')
-  } else {
-    next()
+  try {
+    if (to.meta.requiresAuth && !store.state.authenticated) {
+      next('/Signin')
+    } else {
+      next()
+    }
+  } catch (error) {
+    console.error('Router guard error:', error)
+    next('/Signin') // Redirect to a fallback route if there’s an error
   }
-
-   
-   
-});
-export default router;
+})
+export default router
