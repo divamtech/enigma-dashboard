b8d1af139766f8239f639575b952bfba16c2f44f
YashS0ni <soniyash6606@gmail.com>
2024-07-29 15:15:57 +0530

    (work in progress)

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import store from "./store";
-import router from "./router";
-import "./assets/css/nucleo-icons.css";
-import "./assets/css/nucleo-svg.css";
-import ArgonDashboard from "./argon-dashboard";
-import axios from "axios";
-axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './store'
+import router from './router'
+import './assets/css/nucleo-icons.css'
+import './assets/css/nucleo-svg.css'
+import ArgonDashboard from './argon-dashboard'
+import axios from 'axios'
 
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
 
-const appInstance = createApp(App);
-appInstance.use(store);
-appInstance.use(router);
-appInstance.use(ArgonDashboard);
-appInstance.mount("#app");
+const appInstance = createApp(App)
+appInstance.use(store)
+appInstance.use(router)
+appInstance.use(ArgonDashboard)
+appInstance.mount('#app')
